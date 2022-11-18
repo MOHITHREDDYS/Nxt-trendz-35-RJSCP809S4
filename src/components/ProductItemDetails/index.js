4f18e8674cc54a7da998a44150759b7f55abbb25
MOHITHREDDYS <mohithreddys.531@gmail.com>
2022-11-18 06:17:41 +0000

    code added

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,18 +1,31 @@
 // Write your code here
 import {Component} from 'react'
 import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
 import Header from '../Header'
 import './index.css'
 
+const productApiStatusList = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  loading: 'LOADING',
+}
+
 class ProductItemDetails extends Component {
-  state = {productItem: {}, isLoading: true}
+  state = {
+    productItem: {},
+    apiStatus: productApiStatusList.initial,
+    productQuantity: 1,
+  }
 
   componentDidMount() {
     this.getProductDetails()
   }
 
   getProductDetails = async () => {
-    console.log(this.props)
+    this.setState({apiStatus: productApiStatusList.loading})
     const {match} = this.props
     const {params} = match
     const {id} = params
@@ -50,22 +63,47 @@ class ProductItemDetails extends Component {
         })),
       }
 
-      console.log(formattedData.similarProducts)
-
-      this.setState({productItem: formattedData, isLoading: false})
+      return this.setState({
+        productItem: formattedData,
+        apiStatus: productApiStatusList.success,
+      })
     }
+    return this.setState({apiStatus: productApiStatusList.failure})
+  }
+
+  onClickingMinus = () => {
+    const {productQuantity} = this.state
+    if (productQuantity > 1) {
+      this.setState(prevState => ({
+        productQuantity: prevState.productQuantity - 1,
+      }))
+    }
+  }
+
+  onClickingPlus = () => {
+    this.setState(prevState => ({
+      productQuantity: prevState.productQuantity + 1,
+    }))
+  }
+
+  onClickingContinueShopping = () => {
+    const {history} = this.props
+    history.replace('/products')
   }
 
   getSimilarProduct = () => {
     const {productItem} = this.state
     const {similarProducts} = productItem
-    console.log(similarProducts)
 
     return similarProducts.map(eachProduct => {
       const {imageUrl, title, brand, price, rating} = eachProduct
       return (
-        <li className="smlr-prdt-container">
-          <img src={imageUrl} alt={title} className="smlr-prdt-image" />
+        <li className="smlr-prdt-container" key={eachProduct.id}>
+          <img
+            src={imageUrl}
+            alt="similar product"
+            className="smlr-prdt-image"
+          />
           <p className="smlr-prdt-title">{title}</p>
           <p className="smlr-prdt-brand">by {brand}</p>
           <div className="smlr-prdt-price-rating-container">
@@ -84,9 +122,8 @@ class ProductItemDetails extends Component {
     })
   }
 
-  render() {
-    const {productItem, isLoading} = this.state
-    console.log(productItem)
+  renderProductItem = () => {
+    const {productItem, productQuantity} = this.state
     const {
       imageUrl,
       title,
@@ -99,10 +136,9 @@ class ProductItemDetails extends Component {
     } = productItem
     return (
       <>
-        <Header />
         <div className="item-details-main-container">
           <div className="item-image-details-container">
-            <img src={imageUrl} alt={title} className="item-image" />
+            <img src={imageUrl} alt="product" className="item-image" />
             <div className="item-details-container">
               <h1 className="item-title">{title}</h1>
               <p className="item-price">Rs {price}/-</p>
@@ -119,33 +155,100 @@ class ProductItemDetails extends Component {
               </div>
               <p className="item-description">{description}</p>
               <p className="item-availability-brand">
-                Available:{' '}
                 <span className="item-availability-brand-values">
-                  {availability}
+                  Available:{' '}
                 </span>
+                {availability}
               </p>
               <p className="item-availability-brand">
-                Brand:
-                <span className="item-availability-brand-values">{brand}</span>
+                <span className="item-availability-brand-values">Brand: </span>
+                {brand}
               </p>
               <hr className="item-horizontal-line" />
               <div className="item-quantity-container">
-                <p className="plus-minus-sign">-</p>
-                <p className="item-quantity">1</p>
-                <p className="plus-minus-sign">+</p>
+                <button
+                  type="button"
+                  className="plus-minus-button"
+                  data-testid="minus"
+                >
+                  <BsDashSquare
+                    className="plus-square"
+                    onClick={this.onClickingMinus}
+                  />
+                </button>
+                <p className="item-quantity">{productQuantity}</p>
+                <button
+                  type="button"
+                  className="plus-minus-button"
+                  data-testid="plus"
+                >
+                  <BsPlusSquare
+                    className="plus-square"
+                    onClick={this.onClickingPlus}
+                  />
+                </button>
               </div>
               <button type="button" className="add-to-cart-button">
                 ADD TO CART
               </button>
             </div>
-            <div>
-              <h1 className="smlr-prdt-heading">Similar Products</h1>
-              <ul className="smlr-prdt-list-container">
-                {!isLoading && this.getSimilarProduct()}
-              </ul>
-            </div>
+          </div>
+          <div className="smlr-prdt-main-container">
+            <h1 className="smlr-prdt-heading">Similar Products</h1>
+            <ul className="smlr-prdt-list-container">
+              {this.getSimilarProduct()}
+            </ul>
           </div>
         </div>
+      </>
+    )
+  }
+
+  renderFailureView = () => (
+    <div className="item-error-main-container">
+      <div className="item-error-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+          alt="failure view"
+          className="item-error-image"
+        />
+        <h1 className="item-error-heading">Product Not Found</h1>
+        <button
+          className="item-error-button"
+          type="button"
+          onClick={this.onClickingContinueShopping}
+        >
+          Continue Shopping
+        </button>
+      </div>
+    </div>
+  )
+
+  renderLoadingView = () => (
+    <div data-testid="loader" className="item-loading-spinner">
+      <Loader type="ThreeDots" color="#0b69ff" height={80} width={80} />
+    </div>
+  )
+
+  getDisplayPage = () => {
+    const {apiStatus} = this.state
+    switch (apiStatus) {
+      case productApiStatusList.success:
+        return this.renderProductItem()
+      case productApiStatusList.failure:
+        return this.renderFailureView()
+      case productApiStatusList.loading:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        {this.getDisplayPage()}
       </>
     )
   }
